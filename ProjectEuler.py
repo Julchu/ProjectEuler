662efe44bae6616c79e05dbf128f56a8a42b397f
Julian <jchumtl@gmail.com>
2018-11-21 16:26:38 -0500

    Attempting Q3 prime numbers

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -13,19 +13,38 @@ def Q1(n):
 		i += 1
 	return sum
 
-def Q2():
+def Q2(n):
 	"""
 	Def: sum of even fibonacci under 4,000,000
-	Run: print(Q2())
+	Run: print(Q2(4000000))
 	"""
 	fib = [0, 1]
 	sum = 0
 	i = 1
-	while fib[i] < 4000000:
+	while fib[i] < n:
 		fib.append(fib[i] + fib[i-1])
 		if fib[i] % 2 == 0:
 			sum += fib[i]
 		i += 1
 	return sum
 
-def Q3():
+def Q3(n):
+	"""
+	Def: largest prime factor of 600851475143
+	Run: print(Q3(600851475143))
+	"""
+	factor = n
+
+	i = 1
+	while i < n:
+		j = 1
+		while j < i:
+			if i % j == 0 and :
+				j = i
+				i += 1
+			else:
+				j += 1
+
+		i += 1
+
+	return factor
